942d50ad72e0a6cabdd91293dd8ab8766282b769
Meighel <120162915+Meighel@users.noreply.github.com>
2024-09-18 08:48:53 +0800

    Update practicephp.php

--- practicephp.php
+++ practicephp.php
@@ -8,7 +8,18 @@
 	$person-> name = "Alice";
 	$person-> age = 25;
 	
-	print_r($person)
-
+	print_r($person);
 	
+	var_dump($person);
+
+	$name = 'Brad';
+	$age = 40;
+	$hasKids = true;
+	$cashOnHand = 10.5;
+	
+	var_dump($cashOnHand);
+	
+	echo "$name is $age years old\n";
+	echo "${name} is ${age} years old\n";
+	echo '<h3>' . $name . 'is' . $age . ' years old</h3>'
 ?>
