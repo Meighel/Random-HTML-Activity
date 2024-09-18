93c0aa456cb3c78ee92489f678d2cdbcd22953ff
Meighel <120162915+Meighel@users.noreply.github.com>
2024-09-18 12:17:14 +0800

    Create process-form.php

---
+++ process-form.php
@@ -0,0 +1,16 @@
+<?php 
+
+   if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+      $side1 = floatval($_POST['side1']);
+      $side2 = floatval($_POST['side2']);
+      $side3 = floatval($_POST['side3']);
+
+      $s = ($side1 + $side2 + $side3) / 2;
+
+      $areaSquared = $s * ($s - $side1) * ($s - $side2) * ($s - $side3);
+      $area = pow($areaSquared, 1/2);  
+
+      echo "<div class='result'>The area of the triangle is: " . number_format($area, 2) . " square units</div>";
+        }
+?>
